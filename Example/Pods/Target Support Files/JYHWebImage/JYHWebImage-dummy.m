b47826b8471a7759b9c26dbc88753c4225fcdfee
youhua2021 <jiangyouhua@daorealm.com>
2021-10-11 17:19:45 +0800

    add new file

---
+++ Example/Pods/Target Support Files/JYHWebImage/JYHWebImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JYHWebImage : NSObject
+@end
+@implementation PodsDummy_JYHWebImage
+@end
---
+++ Example/Pods/Target Support Files/Pods-JYHWebImage_Example/Pods-JYHWebImage_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JYHWebImage_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JYHWebImage_Example
+@end
